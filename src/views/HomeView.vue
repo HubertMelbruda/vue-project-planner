72c3765619aa56806c4e87ab23a11a61385b318c
HubertMelbruda <h.melbruda@gmail.com>
2022-05-13 16:27:34 +0200

    Add Axios library and fetch data from database

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+const axios = require("axios").default
 
 export default {
-  name: 'HomeView',
-  components: {
-    
-  }
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:8000/projects").then(res => {
+      this.projects = res.data
+    })
+  },
 }
 </script>
