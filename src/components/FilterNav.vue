431d3a94d47bde7b48e975b80852d889f71859cb
HubertMelbruda <h.melbruda@gmail.com>
2022-05-16 19:45:32 +0200

    Add navbar with filters

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="filter-nav">
+    <button
+      @click="updateFilter('all')"
+      class="filter-nav__button"
+      v-bind:class="{ active: current === 'all' }"
+    >
+      View All
+    </button>
+    <button
+      @click="updateFilter('completed')"
+      class="filter-nav__button"
+      v-bind:class="{ active: current === 'completed' }"
+    >
+      Completed
+    </button>
+    <button
+      @click="updateFilter('inProgress')"
+      class="filter-nav__button"
+      v-bind:class="{ active: current === 'inProgress' }"
+    >
+      In progress
+    </button>
+  </nav>
+</template>
+<script>
+export default {
+  props: ['current'],
+  methods: {
+    updateFilter(filter) {
+      this.$emit('filterChange', filter)
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.filter-nav__button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.active {
+  color: #555;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="currentFilter = $event" :current="currentFilter"/>
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject
           :project="project"
           @deleteProject="handleDeleteProject"
@@ -14,16 +15,19 @@
 
 <script>
 import SingleProject from "../components/SingleProject.vue"
+import FilterNav from "../components/FilterNav.vue"
 const axios = require("axios").default
 
 export default {
   name: "HomeView",
   components: {
     SingleProject,
+    FilterNav,
   },
   data() {
     return {
       projects: [],
+      currentFilter: 'all',
     }
   },
   methods: {
@@ -36,6 +40,17 @@ export default {
       })
       p.complete = !p.complete
     },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.currentFilter === 'completed') {
+        return this.projects.filter(project => project.complete)
+      } 
+      if (this.currentFilter === 'inProgress') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
+    } 
   },
   mounted() {
     axios.get("http://localhost:8000/projects").then(res => {
